a09dca579a72b128cfbc4669a8f5486918e9d923
Mahir <mahir094094@gmail.com>
2026-05-20 21:12:32 +0600

    added session manager

---
+++ SessionManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheatreManagementSystem
+{
+    public static class SessionManager
+    {
+        public static int UserId { get; set; }
+        public static string UserName { get; set; }
+        public static int UserTypeId { get; set; }
+    }
+}
